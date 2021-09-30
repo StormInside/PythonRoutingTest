07e39ba481f7d7cddcd1a98affff6c0ab7d825e9
StormInside <storminside4766@gmail.com>
2021-09-30 20:21:27 +0300

    IP replaced with interface class

--- router.py
+++ router.py
@@ -1,91 +1,98 @@
 import socket
-from ipaddress import ip_address
+from ipaddress import ip_address, ip_network, IPv4Interface
 import threading
 import json
 
 
 class Router:
 
-    def __init__(
-                self,
-                ip="10.0.0.1",
-                broadcast_port=9060,
-                unused_interfaces=[9101,9102,9103,9104],
-                routing_protocol=None
-                ):
-        
-        self.ip = ip_address(ip)
-        self.broadcast_port = broadcast_port
+    def __init__(self, hostname, interfaces):
 
-        self.broadcast_interface = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.broadcast_interface.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self.broadcast_interface.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.broadcast_interface.bind(('localhost', self.broadcast_port))
+        self.hostname = hostname
 
-        self.unused_interfaces = unused_interfaces
+        self.interfaces = interfaces
+        self.ip_list = [str(self.interfaces[i]["interface"].ip) for i in interfaces]
+
+        self.__create_broadcast()
 
         self.data_packet = {"src_ip": "ip", "dst_ip": "ip", "data": "message"}
-        self.broadcast_packet = {"src_ip": str(self.ip), "data": "message"}
+        self.broadcast_packet = {"src_ip": "ip", "data": "message"}
 
         self.connections = {}
         self.threads = {}
 
-        self.routing_protocol = routing_protocol
-        self.routing_table = {}
+        self.routing_table = {ip_network('192.168.0.0/28')}
 
-    def connect_to_router(self, ip):
-        port = self.unused_interfaces[0]
-        if not port:
-            return 0
-        self.unused_interfaces.pop(0)
+    def __create_broadcast(self):
+        for interface in self.interfaces:
+            broadcast_port = self.interfaces[interface]["br_port"]
+            broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            broadcast_socket.bind(('localhost', broadcast_port))
+            self.interfaces[interface]["br_socket"] = broadcast_socket
+
+    def _get_ip_lport(self, l_port):
+        for interface in self.interfaces:
+            if self.interfaces[interface]["l_port"] == l_port:
+                return self.interfaces[interface]["interface"].ip
+
+    def connect_to_router(self, ip, l_port):
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind(('localhost', port))
+        sock.bind(('localhost', l_port))
         sock.listen(1)
-
-        self.connections[port] = {"remote_ip": ip, "remote_port": port, "connection": sock}
-
-        waiter = threading.Thread(target=self.wait_connection, args=(port,))
+        self.connections[l_port] = {"remote_ip": ip, "remote_port": l_port, "connection": sock}
+        # print(sock) #
+        waiter = threading.Thread(target=self.wait_connection, args=(l_port,))
         waiter.start()
-        self.threads[f"{port}_waiter"] = waiter
+        self.threads[f"{l_port}_waiter"] = waiter
 
     def wait_connection(self, port):
         connection = self.connections[port]
         sock = connection["connection"]
         conn, addr = sock.accept()
-        print(f"Router {self.ip} CONNECTED to {connection['remote_ip']}")
+        print(f"Router {self.hostname} CONNECTED to {connection['remote_ip']}")
         self.connections[port]["connection"] = conn
+        # print(conn) #
         listener = threading.Thread(target=self.listen_conn, args=(port,))
         listener.start()
         self.threads[f"{port}_listener"] = listener
 
-    def accept_connection(self, port, ip):
-        connection_port = self.unused_interfaces[0]
-        self.unused_interfaces.pop(0)
+    def accept_connection(self, r_port, l_port, ip):
         connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        connection.connect(('localhost', port))
-        self.connections[connection_port] = {"remote_ip": ip, "remote_port": port, "connection": connection}
-        listener = threading.Thread(target=self.listen_conn, args=(connection_port,))
+        connection.connect(('localhost', r_port))
+        self.connections[l_port] = {"remote_ip": ip, "remote_port": r_port, "connection": connection}
+        # print(connection) #
+        listener = threading.Thread(target=self.listen_conn, args=(l_port,))
         listener.start()
-        self.threads[f"{connection_port}_listener"] = listener
+        self.threads[f"{l_port}_listener"] = listener
 
     def message_to_connection(self, message, local_port):
         connection = self.connections[local_port]
         conn = connection["connection"]
         packet = self.data_packet
         packet["data"] = message
-        packet["src_ip"] = str(self.ip)
+        packet["src_ip"] = str(self._get_ip_lport(local_port))
         packet["dst_ip"] = str(connection["remote_ip"])
         packet = json.dumps(packet)
         conn.send(bytes(packet, "utf-8"))
 
+    def message_to_broadcast(self, message, interface="All"):
+        def send_data(interface):
+            sock = self.interfaces[interface]["br_socket"]
+            port = self.interfaces[interface]["br_port"]
+            packet = self.broadcast_packet
+            packet["src_ip"] = str(self.interfaces[interface]["interface"].ip)
+            packet["data"] = message
+            packet = json.dumps(packet)
+            sock.sendto(bytes(packet, "utf-8"), ('<broadcast>', port))
 
-
-    def message_to_broadcast(self, message):
-        packet = self.broadcast_packet
-        packet["data"] = message
-        packet = json.dumps(packet)
-        self.broadcast_interface.sendto(bytes(packet, "utf-8"), ('<broadcast>', self.broadcast_port))
+        if interface != "All":
+            send_data(interface)
+        else:
+            for interface in self.interfaces:
+                send_data(interface)
 
     def listen_conn(self, port):
         connection = self.connections[port]["connection"]
@@ -95,24 +102,25 @@ class Router:
             if not message:
                 break
 
-            print(f"Router {self.ip} gets '{message}'")
+            print(f"Router {self.hostname} gets '{message}'")
 
     def listen_broadcast(self):
-        def listener():
+        def listener(sock):
             while True:
-                data, addr = self.broadcast_interface.recvfrom(1024)
+                data, addr = sock.recvfrom(1024)
                 data = data.decode("utf-8")
                 data = json.loads(data)
-                if data["src_ip"] != str(self.ip):
+                if data["src_ip"] not in self.ip_list:
                     self.parse_broadcast_data(data, addr)
 
-        broadcast_listener = threading.Thread(target=listener)
-        broadcast_listener.start()
-        self.threads["broadcast_listener"] = broadcast_listener
+        for interface in self.interfaces:
+            broadcast_listener = threading.Thread(target=listener, args=(self.interfaces[interface]["br_socket"], ))
+            broadcast_listener.start()
+            self.threads[f"broadcast_listener{interface}"] = broadcast_listener
 
     def parse_broadcast_data(self, data, addr):
-        print(f"Router {self.ip} received '{data}' from broadcast {addr}")
+        print(f"Router {self.hostname} received '{data}' from broadcast {addr}")
 
     def start(self):
         self.listen_broadcast()
-        print(f"Router {self.ip} have started")
+        print(f"Router {self.hostname} have started")
--- simulation.py
+++ simulation.py
@@ -1,6 +1,7 @@
 from tabulate import tabulate
 import threading
 import time
+from ipaddress import ip_address, ip_network, IPv4Interface
 
 from router import Router
 
@@ -10,8 +11,8 @@ class Simulation:
     def __init__(self):
         self.routers = {}
 
-    def add_router(self, router_name, router_ip, broadcast_port, unused_interfaces):
-        self.routers[router_name] = Router(router_ip, broadcast_port, unused_interfaces)
+    def add_router(self, router_name, router_interfaces, broadcast_port):
+        self.routers[router_name] = Router(router_name, router_interfaces)
 
     def start_routers(self):
         for router in self.routers:
@@ -21,9 +22,24 @@ class Simulation:
 if __name__ == "__main__":
     sim = Simulation()
 
-    sim.add_router("router1", "10.0.1.1", 9060, [9101, 9102, 9103, 9104])
-    sim.add_router("router2", "10.0.2.1", 9060, [9201, 9202, 9203, 9204])
-    sim.add_router("router3", "10.0.3.1", 9060, [9301, 9302, 9303, 9304])
+    r1_interfaces = {0: {"l_port": 9101, "br_port": 9910, "interface": IPv4Interface('10.1.1.1/24')},
+                     1: {"l_port": 9102, "br_port": 9920, "interface": IPv4Interface('10.1.2.1/24')},
+                     2: {"l_port": 9103, "br_port": 9930, "interface": IPv4Interface('10.1.3.1/24')},
+                     3: {"l_port": 9104, "br_port": 9940, "interface": IPv4Interface('10.1.4.1/24')}}
+
+    r2_interfaces = {0: {"l_port": 9201, "br_port": 9910, "interface": IPv4Interface('10.2.1.1/24')},
+                     1: {"l_port": 9202, "br_port": 9920, "interface": IPv4Interface('10.2.2.1/24')},
+                     2: {"l_port": 9203, "br_port": 9930, "interface": IPv4Interface('10.2.3.1/24')},
+                     3: {"l_port": 9204, "br_port": 9940, "interface": IPv4Interface('10.2.4.1/24')}}
+
+    r3_interfaces = {0: {"l_port": 9301, "br_port": 9910, "interface": IPv4Interface('10.3.1.1/24')},
+                     1: {"l_port": 9302, "br_port": 9920, "interface": IPv4Interface('10.3.2.1/24')},
+                     2: {"l_port": 9303, "br_port": 9930, "interface": IPv4Interface('10.3.3.1/24')},
+                     3: {"l_port": 9304, "br_port": 9940, "interface": IPv4Interface('10.3.4.1/24')}}
+
+    sim.add_router("router1", r1_interfaces, 9060)
+    sim.add_router("router2", r2_interfaces, 9060)
+    sim.add_router("router3", r3_interfaces, 9090)
 
     time.sleep(1)
 
@@ -33,11 +49,11 @@ if __name__ == "__main__":
 
     time.sleep(1)
 
-    sim.routers["router1"].connect_to_router("10.0.2.1")
-    sim.routers["router2"].accept_connection(9101, "10.0.1.1")
-    sim.routers["router2"].connect_to_router("10.0.3.1")
-    sim.routers["router3"].accept_connection(9202, "10.0.2.1")
-
+    sim.routers["router1"].connect_to_router("10.0.2.1", 9101)
+    sim.routers["router2"].accept_connection(9101, 9201, "10.0.1.1")
+    sim.routers["router2"].connect_to_router("10.0.3.1", 9202)
+    sim.routers["router3"].accept_connection(9202, 9301, "10.0.2.1")
+    #
     sim.routers["router2"].message_to_connection("HELLO", 9201)
     time.sleep(1)
     sim.routers["router1"].message_to_connection("REPLY", 9101)
@@ -45,9 +61,10 @@ if __name__ == "__main__":
     sim.routers["router2"].message_to_connection("HELLO", 9202)
     time.sleep(1)
     sim.routers["router3"].message_to_connection("REPLY", 9301)
-
-    sim.routers["router1"].message_to_broadcast("Hello, can you hear?")
-    sim.routers["router2"].message_to_broadcast("Hello, I can hear")
+    #
+    # sim.routers["router1"].message_to_broadcast("Hello, can you hear?", 0)
+    # sim.routers["router2"].message_to_broadcast("Hello, I can hear", 1)
+    # sim.routers["router2"].message_to_broadcast("Hello, I can hear")
 
 
 
